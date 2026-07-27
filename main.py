5d35f114a7111ff6340ac89f921200e7a78dda45
Asmit Potdar <ashmit.s.potdar@gmail.com>
2026-07-27 04:36:14 +0400

    Added functionality

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import argparse
+import pathlib
 
 
 
+FILE_RULES = {
+    "Documents":[".pdf",".txt",".docx"],
+    "Archives": [".zip", ".7z", ".rar"],
+    "Images": [".png",".jpg",".jpgeg",".webp",".gif"]
+}
 #parser letting us run the script with extra functionality 
 
 parser = argparse.ArgumentParser(description="Directory Cleaner")
@@ -13,7 +19,15 @@ parser.add_argument("-ext", help="Clean up certain files, eg:pngs")
 args = parser.parse_args()
 
 if args.dry_run:
-    print("dry-run mode is now active")
+    print("Dry-run mode is now active")
 if args.ext:
     print(f"Cleaning for {args.ext.lstrip(".")} files")
-    
+
+#gets the category for the input file extension
+def get_category(extension: str) -> str:
+    for category in FILE_RULES:
+        if extension in FILE_RULES[category]:
+            return category
+
+    return f"{extension} not found in file categories"
+
